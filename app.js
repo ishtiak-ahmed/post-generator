782ad727d92d2368f3146631dede0c475f6fac16
Ishtiak Ahmed <iahmed1606@gmail.com>
2021-02-16 22:08:55 +0600

    Live link updated

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ function generateContent() {
     const text = (document.getElementById('phoneModel').value);
     const model = text.toUpperCase()
 
-    const postModel = `Download ${model} Flash File (Stock Official Ram) free. We are bringing you ${model} flash file without any password. You can download flash file for free. With direct link and fast download server. Thank you for visiting our website. If you need any other flash file check our website bdtechpoint.com 
-Before downloading the ${model} flash file, carefully check your phone model. Thanks for downloading the file from us. I Hope to see you again. 
+    const postModel = `Download <strong>${model}</strong> Flash File (Stock Official Ram) free. We are bringing you <strong>${model}</strong> flash file without any password. You can download flash file for free. With direct link and fast download server. Thank you for visiting our website. If you need any other flash file check our website bdtechpoint.com 
+    <br>
+    <br>
+You are going to download <strong>${model}</strong> flash file, carefully check your phone model. Thanks for downloading the file from us. I Hope to see you again. 
 `
     document.getElementById('result').innerHTML = postModel;
 }
