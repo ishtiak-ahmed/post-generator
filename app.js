d52ed4006dc799cae2fb9a857fc32ff5290e30c0
Ishtiak Ahmed <iahmed1606@gmail.com>
2021-02-16 21:57:59 +0600

    Post content updated

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+const text = `Download ${model} Flash File (Stock Official Ram) free. We are bringing you ${model} flash file without any password. You can download flash file for free. With direct link and fast download server. Thank you for visiting our website. If you need any other flash file check our website bdtechpoint.com 
+Before downloading the ${model} flash file, carefully check your phone model. Thanks for downloading the file from us. I Hope to see you again. 
+`
+
+
 function generateContent() {
     const text = (document.getElementById('phoneModel').value);
     const model = text.toUpperCase()
-    const postModel = `Download <strong>${model}</strong> Flash File SC6531E Tested (Stock Official Rom). I'm sure you were looking for a <strong>${model}</strong> Flash File software. At any one time you have come to our bdtechpoint.com site to search for your <strong>${model}</strong> Flash File software. Many thanks and best wishes to you for visiting our site. If you have come to our site for this <strong>${model}</strong> Flash File software. Then, we'll assume you've chosen the right software site. Because we always upload original and official flash file software on our site. We never upload any bad Flash file software to our site. We never worry about anyone being harassed for downloading files from our site. You can download the mobile flash file from our site without any worries.
 
-    <br>
-    <br>
-    When downloading the <strong>${model}</strong> Flash File software from our site, you must verify your device model and brand. Because it turned out that you were looking for "Samsung Flash File or Oppo Flash File" software but the page you came across is "Huawei Flash File" software so you may be a victim of harassment. 
-    So, I would request you to make sure that the mobile flash file software that you have come to search for matches. If there is no match, you can search and download your mobile Flash file software in the "Search" option from the navigation bar at the top of our site.`
-
+    const text = `Download ${model} Flash File (Stock Official Ram) free. We are bringing you ${model} flash file without any password. You can download flash file for free. With direct link and fast download server. Thank you for visiting our website. If you need any other flash file check our website bdtechpoint.com 
+Before downloading the ${model} flash file, carefully check your phone model. Thanks for downloading the file from us. I Hope to see you again. 
+`
     document.getElementById('result').innerHTML = postModel;
 }
 
