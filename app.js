180acbcd9397c3c6a02312db3fd5ec2cd88c1d07
Ishtiak Ahmed <iahmed1606@gmail.com>
2021-02-16 22:04:36 +0600

    Updated post function

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function generateContent() {
     const text = (document.getElementById('phoneModel').value);
     const model = text.toUpperCase()
 
-    const text = `Download ${model} Flash File (Stock Official Ram) free. We are bringing you ${model} flash file without any password. You can download flash file for free. With direct link and fast download server. Thank you for visiting our website. If you need any other flash file check our website bdtechpoint.com 
+    const postModel = `Download ${model} Flash File (Stock Official Ram) free. We are bringing you ${model} flash file without any password. You can download flash file for free. With direct link and fast download server. Thank you for visiting our website. If you need any other flash file check our website bdtechpoint.com 
 Before downloading the ${model} flash file, carefully check your phone model. Thanks for downloading the file from us. I Hope to see you again. 
 `
     document.getElementById('result').innerHTML = postModel;
